3f1bb4977e91b2821992770f0d072a0bb176d902
Nusrat Jahan <nusrat@nusrats-mbp.myfiosgateway.com>
2020-09-16 20:34:36 -0400

    First home work

---
+++ src/class-2/Homework.java
@@ -0,0 +1,27 @@
+package Class2;
+
+public class Homework {
+
+    public static void main(String [] args){
+       byte noOfHours = 24;
+       System.out.println("Number of hours in a day is " + noOfHours);
+       short noOfDays = 365;
+       System.out.println("Maximum number of days in a year is " + noOfDays);
+       int noOfEmployees = 114096;
+       System.out.println("Total number of google employees are " + noOfEmployees);
+       int noOfPopulation = 331002651;
+       System.out.println("Total number of US population is " + noOfPopulation);
+       float interestRate = 3.25f;
+       System.out.println("Mortgage loan interest rate is " + interestRate);
+       int accountBalance = 100000;
+       System.out.println("Bank account balance is " + accountBalance);
+       boolean sunRiseFromTheWest = false;
+       System.out.println("Does the sun rise from the west? : " + sunRiseFromTheWest );
+       String myName = "Nusrat Jahan";
+       System.out.println("My name is " + myName);
+       String  nameInitial = "NJ";
+       System.out.println("Initial of my name is "+ nameInitial);
+
+
+}
+}
